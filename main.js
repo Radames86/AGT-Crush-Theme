e5425519681afcc26441070004a42bab613d10e2
Radames Rivera <rrivera@perseverenow.org>
2025-07-14 10:29:06 -0500

    second

--- main.js
+++ main.js
@@ -27,6 +27,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     createBoard() // now we call the function to actually build the board and see if it works
 
+    function dragStart(){
+        faceBeingDragged = this.style.backgroundImage //faceBeingDropped has the faces of the judges stored in it's arr. this refers to the selected div/square that was just clicked and started being dragged
+        squareIdBeingDragged = parseInt(this.id) // this.id gets the id of the attr. of the square being dragged
+    }
+
+    function dragOver(e){ //this function will run when a dragged item is moved over a drop target
+        e.preventDefault() //this function allows the dragged item to be dropped on the target square
+    }
+    // both dragOver and dragEnter functions work together to allow the drop action
+
+    function dragEnter(e){ // this runs when the dragged item enters a drop target
+        e.preventDefault() // this allows the drop e to work on this square
+    }
+    
+    function dragDrop(){ // this runs when you drop the dragged face onto another square
+        faceBeingReplaced = this.style.backgroundImage // 
+        squareBeingReplaced = parseInt(this.id) // gets the id of the square and converts it to a number for future use
+
+        this.style.backgroundImage = faceBeingDragged
+        squares[squareIdBeingDragged].style.backgroundImage = faceBeingReplaced // we set the square of the face dragged from to show the face replaced, swaps the 2 faces
+    }
+
+    function dragEnd(){ // runs when the drag operation finishes
+
+    }
+
+    squares.forEach(square => { // loops through each square in the squares arr. it listens for certain drag and drop e and calls the functions when those e happen
+        square.addEventListener('dragstart', dragStart) // when player starts dragging a square dragStart runs
+        square.addEventListener('dragover', dragOver) // when dragged item is moved over a square dragOver runs
+        square.addEventListener('dragenter', dragEnter) // when a dragged item enters a square dragEnter runs
+        square.addEventListener('dragdrop', dragDrop) // when the dragged item is dropped on a square dragDrop runs
+        square.addEventListener('dragend', dragEnd) // when the drag finishes dragEnd runs
+    })
+
+    
    
 })
 // this is part of my boiler plate set up
